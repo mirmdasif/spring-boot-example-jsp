812c93bafe5fe02bcc25cce9017bcf6a57f70d13
Mir Md. Asif Hossain <asif.hossasin@therapservices.net>
2015-01-25 15:57:39 +0600

    Initialize a gradle project serving jsp

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+public class Application extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        return "test";
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
